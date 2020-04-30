282d739eb95fc6037ad2ef86850ad119119808b9
Rubiksron <rubiksron@gmail.com>
2020-04-29 17:54:31 -0700

    showing bikepaths at users location.

--- app.js
+++ app.js
@@ -1,53 +1,72 @@
 'use strict';
 
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 14,
-//     center: {lat: 42.3726399, lng: -71.1096528}
-//   });
-
-//   var bikeLayer = new google.maps.BicyclingLayer();
-//   console.log('bikeLayer', bikeLayer);
-//   bikeLayer.setMap(map);
-// }
-
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-var map, infoWindow;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: {lat: 42.3726399, lng: -71.1096528}
   });
-  infoWindow = new google.maps.InfoWindow;
+  var bikeLayer = new google.maps.BicyclingLayer();
+  // var infoWindow = new google.maps.InfoWindow;
 
-  // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
+      // bikeLayer.setPosition(pos);
+      // infoWindow.setContent('Location found.');
+      bikeLayer.setMap(map);
       map.setCenter(pos);
+      logPosition(pos);
     }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
+      handleLocationError(true, bikeLayer, map.getCenter());
     });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
   }
+
 }
+
+
+
+// var map, infoWindow, pos;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 42.3726399, lng: -71.1096528},
+//     zoom: 13
+//   });
+//   infoWindow = new google.maps.InfoWindow;
+
+//   // Try HTML5 geolocation.
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       var pos = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude
+//       };
+//       infoWindow.setPosition(pos);
+//       infoWindow.setContent('Location found.');
+//       infoWindow.open(map);
+//       map.setCenter(pos);
+//       logPosition(pos);
+//     }, function() {
+//       handleLocationError(true, infoWindow, map.getCenter());
+//     });
+//   } else {
+//     // Browser doesn't support Geolocation
+//     handleLocationError(false, infoWindow, map.getCenter());
+//   }
+// }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
+
+function logPosition(pos) {
+  console.log('logPosisiton: ', pos);
+}
+
